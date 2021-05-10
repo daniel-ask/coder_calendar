3668eb0c1d73d80dd7ec4967d362f952d34303a0
Daniel Ask <cdanielask@gmail.com>
2021-05-10 21:58:36 +1000

    Initial commit

---
+++ app/controllers/calendar_controller.rb
@@ -0,0 +1,9 @@
+class CalendarController < ApplicationController
+  def calendar
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @meetings = Event.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+  end
+
+  def list
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,43 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+names = %w[
+  Yannick
+  Sainath
+  Mark
+  Deeon
+  Vanessa
+  Karen
+  Murphy
+  Joanna
+  Peter
+  Kim
+  Alice
+  Dean
+  Ananda
+  Matthew
+  Jackie
+  Suz
+  Chris
+  Jordan
+  Ryan
+]
+start_time = Date.parse('10-05-2021 14:00')
+end_time = Date.parse('10-05-2021 14:10')
+until names.empty?
+  unless start_time.between?(Date.parse('05-06-2021'), Date.parse('13-06-2021'))
+    student = names.sample
+    Event.create(name: "#{student}'s Lightning Talk", start_time: start_time, end_time: end_time)
+    names.delete(student)
+    student2 = names.sample
+    Event.create(name: "#{student2}'s Lightning Talk", start_time: start_time + 2.days, end_time: end_time + 2.days) unless student2.nil?
+    names.delete(student2)
+  end
+  start_time += 1.week
+  end_time += 1.week
+end
